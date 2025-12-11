472b0d390ff9fb4002f6885750d51dcee4456434
Peter H. Boling <peter.boling@gmail.com>
2025-12-11 13:19:17 -0700

    🎨 kettle-dev template

--- lib/markdown/merge/version.rb
+++ lib/markdown/merge/version.rb
@@ -2,6 +2,11 @@
 
 module Markdown
   module Merge
-    VERSION = "0.1.0"
+    # Version information for Markdown::Merge
+    module Version
+      # Current version of the markdown-merge gem
+      VERSION = "1.0.0"
+    end
+    VERSION = Version::VERSION # traditional location
   end
 end
